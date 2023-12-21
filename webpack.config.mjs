47d3ab58011a02e57da26e185e71fb06fc8aa32d
hellogirls-js <neeneemille@gmail.com>
2023-12-20 20:44:53 -0600

    fix: :bug: env variables should work now

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,5 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import Dotenv from "dotenv-webpack";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -32,6 +33,7 @@ export default {
   optimization: {
     minimize: true,
   },
+  plugins: [new Dotenv()],
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "src/scripts"),
